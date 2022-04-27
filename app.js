c2dce4dfc5af6d7e8ec205bebe964c6440b9257a
Niki Hite <nikihite@comcast.net>
2022-04-26 21:11:14 -0700

    face feature working

--- app.js
+++ app.js
@@ -21,25 +21,60 @@ shedButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'shed');
+    console.log('the hiding spot is', hidingSpot);
+
+    
 });
 
 treeButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'tree');
+    console.log('the hiding spot is', hidingSpot);
 });
 
 boulderButton.addEventListener('click', () => {
     const hidingSpot = Math.floor(Math.random() * 3);
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'boulder');
+    console.log('the hiding spot is', hidingSpot);
 });
 
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
+    shedContainer.classList.remove('face');
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
+
     // then increment the guesses
+    if (userGuess === true || userGuess === false) {
+        totalGuesses++;
+    }
+
     // then grab the appropriate container element for the correct guess from the DOM
+    document.getElementById(`${correctSpot}-container`).classList.add('face');
     // then add the face class to that element so that the face shows up
+    if (correctSpot === 'shed') {
+        shedContainer.classList.add('face');
+        treeContainer.classList.remove('face');
+        boulderContainer.classList.remove('face');
+    } else if (correctSpot === 'tree') {
+        treeContainer.classList.add('face');
+        shedContainer.classList.remove('face');
+        boulderContainer.classList.remove('face');
+    } else {
+        boulderContainer.classList.add('face');
+        treeContainer.classList.remove('face');
+        shedContainer.classList.remove('face');
+    }
+
     // then if the user guess is correct, increment the correct guesses
-    // update the DOM to show this change to the user (including the losses, not tracked directly in state)
-}
+    if (userGuess === true) {
+        correctGuesses++;
+    } else if {
+        totalGuesses - correctGuesses;
+    }
+    // update the DOM to show this change to the user (including the losses, not tracked directly in state);
+winsEl.textContent = correctGuesses;
+totalEl.textContent = totalGuesses;
+lossesEl.textContent = totalGuesses - correctGuesses;
